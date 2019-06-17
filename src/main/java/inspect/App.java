77d633fa678c6df4de707f3e294d03811ca047e7
Francesco Bigozzi <francesco.bigozzi>
2019-06-17 10:02:05 +0200

    Meno priorità alle entity locali

--- src/main/java/inspect/App.java
+++ src/main/java/inspect/App.java
@@ -45,8 +45,8 @@ public final class App {
 
         JsonSchemaGenerator schemaGen = new JsonSchemaGenerator(mapper);
 
-        Reflections reflections = new Reflections("it.arubapec.esecurity.docflysharedapi.domain",
-                "it.arubapec.esecurity.docflydomain.dto.generic", "it.arubapec.esecurity.docflydomain.dto.archive", "inspect.entity",
+        Reflections reflections = new Reflections("inspect.entity", "it.arubapec.esecurity.docflysharedapi.domain",
+                "it.arubapec.esecurity.docflydomain.dto.generic", "it.arubapec.esecurity.docflydomain.dto.archive",
                 new SubTypesScanner(false));
         Set<Class<?>> pojos = reflections.getSubTypesOf(Object.class);
         Map<String, String> schemaByClassNameMap = pojos.stream()
